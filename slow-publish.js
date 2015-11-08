d81c5cee3a3426f3b26bc2c7bffe3557a02a20fe
Mitar <mitar.git@tnode.com>
2015-11-07 22:37:54 -0800

    Initial version.

---
+++ slow-publish.js
@@ -0,0 +1,51 @@
+Documents = new Meteor.Collection('Documents');
+
+if (Meteor.isClient) {
+  Meteor.startup(function () {
+    Meteor.subscribe('last-document', {onStop: function (error) {console.log(error)}});
+  });
+  
+  Template.hello.helpers({
+    latest: function () {
+      // Comment this line out to make subscription fast.
+      return Documents.findOne({}, {sort: {createdAt: -1}, limit: 1});
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      var start = new Date().valueOf();
+      Meteor.subscribe('documents', {
+        onReady: function () {
+          var stop = new Date().valueOf();
+          console.log("Subscription took", ((stop - start) / 1000) + " seconds");
+        },
+        onStop: function (error) {console.log(error)}
+      });
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Documents.find().count() === 0) {
+      for (var i = 0; i < 500; i++) {
+        Documents.insert({
+          createdAt: new Date(),
+        });
+      }
+    }
+  });
+
+  Documents._ensureIndex({createdAt: 1});
+
+  Meteor.publish('last-document', function () {
+    return Documents.find({}, {sort: {createdAt: -1}, limit: 1});
+  });
+
+  Meteor.publish('documents', function () {
+    return Documents.find({});
+  });
+  
+  Facts.setUserIdFilter(function () { return true; });
+}
